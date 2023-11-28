81a2091e63ca9bec3a0e9bff3b6b5be283bb9830
mrs-cow <152328310+mrs-cow@users.noreply.github.com>
2023-11-28 20:49:11 +0000

    My first website

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('perla')
+.addEventListener('click',function() {
+  if (this.style.color ==='white'){
+    this.style.color ='black'
+  } else {
+    this.style.color ='white'
+  }
+})
